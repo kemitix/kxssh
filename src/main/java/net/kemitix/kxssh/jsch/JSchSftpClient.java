49d5b14ca1e26bc79960d7bc0d1e20fb6df1d22c
Paul Campbell <pcampbell@kemitix.net>
2015-03-27 19:55:07 +0000

    JSchSftpClient: make testable
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/kxssh/jsch/JSchSftpClient.java
+++ src/main/java/net/kemitix/kxssh/jsch/JSchSftpClient.java
@@ -1,22 +1,25 @@
 package net.kemitix.kxssh.jsch;
 
 import java.io.File;
+import lombok.Setter;
 import net.kemitix.kxssh.SftpClient;
 import net.kemitix.kxssh.SshConnectionProperties;
 import net.kemitix.kxssh.SshDownload;
 import net.kemitix.kxssh.SshException;
 
+@Setter
 public class JSchSftpClient implements SftpClient {
 
     private final SshConnectionProperties connectionProperties;
+    private SshDownload download;
 
     public JSchSftpClient(SshConnectionProperties connectionProperties) {
         this.connectionProperties = connectionProperties;
+        download = new JSchDownload(connectionProperties);
     }
 
     @Override
     public void download(String remoteFilename, File localFile) throws SshException {
-        SshDownload download = new JSchDownload(connectionProperties);
         download.download(remoteFilename, localFile);
     }
 
