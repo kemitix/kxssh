36f84b7b4ae9b31964273982b85b5844a1b24307
Paul Campbell <pcampbell@kemitix.net>
2015-03-30 16:13:55 +0100

    JSchIOChannelTest: add test for readMetaData() method
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/test/java/net/kemitix/kxssh/jsch/JSchIOChannelTest.java
+++ src/test/java/net/kemitix/kxssh/jsch/JSchIOChannelTest.java
@@ -183,4 +183,63 @@ public class JSchIOChannelTest {
         //then
     }
 
+    /**
+     * Test of readMetaData method, of class JSchIOChannel.
+     *
+     * @throws net.kemitix.kxssh.SshException
+     * @throws java.io.IOException
+     */
+    @Test(timeout = 100L)
+    public void testReadMetaData() throws SshException, IOException {
+        System.out.println("readMetaData");
+        //given
+        ioChannel.setInput(input);
+        ioChannel.setReadReplyFactory(readReplyFactory);
+
+        when(input.read(any(), eq(0), eq(5))).thenReturn(5);
+        IOChannelReadReply headerReply = mock(IOChannelReadReply.class);
+        byte[] headerBuffer = new byte[4];
+        headerBuffer[0] = '0';
+        headerBuffer[1] = '7';
+        headerBuffer[2] = '6';
+        headerBuffer[3] = '4';
+        when(headerReply.getBuffer()).thenReturn(headerBuffer);
+
+        when(input.read(any(), eq(0), eq(1))).thenReturn(1);
+        IOChannelReadReply filesizeDigitReply = mock(IOChannelReadReply.class);
+        byte[] filesizeDigitBuffer = new byte[1];
+        filesizeDigitBuffer[0] = '7';
+        when(filesizeDigitReply.getBuffer()).thenReturn(filesizeDigitBuffer);
+
+        IOChannelReadReply filesizeDelimiterReply = mock(IOChannelReadReply.class);
+        byte[] filesizeDelimiterBuffer = new byte[1];
+        filesizeDelimiterBuffer[0] = ' ';
+        when(filesizeDelimiterReply.getBuffer()).thenReturn(filesizeDelimiterBuffer);
+
+        IOChannelReadReply filenameCharacterReply = mock(IOChannelReadReply.class);
+        byte[] filenameCharacterBuffer = new byte[1];
+        filenameCharacterBuffer[0] = 'f';
+        when(filenameCharacterReply.getBuffer()).thenReturn(filenameCharacterBuffer);
+
+        IOChannelReadReply filenameDelimiterReply = mock(IOChannelReadReply.class);
+        byte[] filenameDelimiterBuffer = new byte[1];
+        filenameDelimiterBuffer[0] = 0x0a;
+        when(filenameDelimiterReply.getBuffer()).thenReturn(filenameDelimiterBuffer);
+
+        when(readReplyFactory.createReply(eq(5), eq(5), any()))
+                .thenReturn(headerReply);
+        when(readReplyFactory.createReply(eq(1), eq(1), any()))
+                .thenReturn(filesizeDigitReply)
+                .thenReturn(filesizeDelimiterReply)
+                .thenReturn(filenameCharacterReply)
+                .thenReturn(filenameDelimiterReply);
+
+        //when
+        IOChannelMetadata metadata = ioChannel.readMetaData();
+
+        //then
+        assertThat(metadata.getHeader(), is(headerBuffer));
+        assertThat(metadata.getFilesize(), is(7));
+        assertThat(metadata.getFilename(), is("f"));
+    }
 }
