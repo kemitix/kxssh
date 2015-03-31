064a4cc187666a8597f9dcbfbfc11f1a9716a399
Paul Campbell <pcampbell@kemitix.net>
2015-03-31 22:27:53 +0100

    JSchOperation.releaseIOChannel(): disconnect ioChannel
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/kxssh/jsch/JSchOperation.java
+++ src/main/java/net/kemitix/kxssh/jsch/JSchOperation.java
@@ -56,6 +56,7 @@ public abstract class JSchOperation implements StatusProvider {
 
     protected void releaseIOChannel() {
         if (ioChannel != null) {
+            ioChannel.disconnect();
             ioChannel = null;
         }
     }
